1555d653dba43066eb228a00c1e6ca91aed0d049
Stephen-Hrrll <stephenhrrll8@gmail.com>
2025-02-13 22:58:56 -0500

    implement TextNode and TextType

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import *
+
+def main():
+    tNode = TextNode("This is a test node", TextType.BOLD_TEXT, "https://www.url.com")
+    print(tNode)
+
+
+main()
+# if __name__ == "__name__":
+#     main()
---
+++ src/textnode.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "Normal Text"
+    BOLD_TEXT = "Bold Text"
+    ITALIC_TEXT = "Italic Text"
+    CODE_TEXT = "Code Text"
+    LINKS = "Links"
+    IMAGES = "Images"
+
+
+
+class TextNode:
+    def __init__(self, text, text_type:TextType , url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        text = self.text == other.text
+        tType = self.text_type == other.text_type
+        url = self.url == other.url
+
+        if text and tType and url:
+            return True
+        
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
